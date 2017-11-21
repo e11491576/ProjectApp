1f8d1c41a2b338716e8237154f33a842638a0e58
THaoChen <es921155@gmail.com>
2017-11-21 13:33:21 +0800

    check

--- app/src/main/java/com/example/kevin/projectapp/DatabaseHelper.java
+++ app/src/main/java/com/example/kevin/projectapp/DatabaseHelper.java
@@ -8,11 +8,25 @@ import android.database.sqlite.SQLiteOpenHelper;
  * Created by programming on 2017/11/14.
  */
 
+
 public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final int VERSION = 1;
+    // tableName
+    public static final String tableName = "account";
+    //id
+    public static final String keyID = "_id";
+    //remain _column
+    public static final String dataTime_column = "time";
+    public static final String term_column = "term";
+    public static final String amount_column="amount";
+    public static final String comsumeLocation_column="comsumeLocation";
+    //create table
+    public static final String createTable =
+            "CREATE TABLE" + tableName + "(" + keyID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    dataTime_column + "DEFAULT CURRENT_TIMESTAMP," + term_column + "TEXT," +
+                    amount_column + "INTEGER NOT NULL," + comsumeLocation_column + "TEXT)";
 
     public static final String DB_name = "myaccount.db";
-
-    public static final int VERSION = 1;
 
     private static SQLiteDatabase database;
 
