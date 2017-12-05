f90f780c7225e0686a014d53589094385691af2d
Kevin <e11491576@gmail.com>
2017-12-05 14:17:08 +0800

    Spinner_Item_Finished

--- app/src/main/java/com/example/kevin/projectapp/AddActivity.java
+++ app/src/main/java/com/example/kevin/projectapp/AddActivity.java
@@ -1,78 +1,49 @@
 package com.example.kevin.projectapp;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.database.Cursor;
-import android.location.Location;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class AddActivity extends AppCompatActivity {
     DatabaseHelper myDb;
-    EditText editTime, editTerm, editAmount;
+    EditText editTerm, editAmount, editLocation;
+    Spinner itemSpinner;
     Button btnSubmit;
-    TextView currentTime, editLocation;
+    TextView currentTime;
     SimpleDateFormat formatter = new SimpleDateFormat("yyyy年MM月dd日HH:mm:ss");
-    Date curDate = new Date(System.currentTimeMillis()); // 獲取當前時間
+    Date curDate = new Date(System.currentTimeMillis()) ; // 獲取當前時間
     String str = formatter.format(curDate);
-    private FusedLocationProviderClient mFusedLocationClient;
-    private String currentLocation =  "123";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
         myDb = new DatabaseHelper(this);
 
-        editTerm = (EditText) findViewById(R.id.editText2);
+
+      //  editTerm = (EditText) findViewById(R.id.editText2);
         editAmount = (EditText) findViewById(R.id.editText3);
-        editLocation = (TextView) findViewById(R.id.textView3);
+        editLocation = (EditText) findViewById(R.id.editText4);
         btnSubmit = (Button) findViewById(R.id.button2);
         currentTime=(TextView)findViewById(R.id.textView);
 
+        itemSpinner=(Spinner)findViewById(R.id.spinner_item);
+        ArrayAdapter<CharSequence> nAdapter = ArrayAdapter.createFromResource(
+                this, R.array.item_array, android.R.layout.simple_spinner_item );
+        itemSpinner.setAdapter(nAdapter);
 
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-                // Got last known location. In some rare situations this can be null.
-                if (location != null) {
-                    // Logic to handle location object
-                    currentLocation += String.valueOf(location.getLongitude());
-                    currentLocation += String.valueOf(location.getLatitude());
-                }
-            }
-        });
-        Toast.makeText(AddActivity.this, currentLocation, Toast.LENGTH_LONG).show();
-        editLocation.setText(currentLocation);
         currentTime.setText(str);//set_curret_time
         AddData();
     }
@@ -83,7 +54,7 @@ public class AddActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
                 boolean isInserted =  myDb.insertData(currentTime.getText().toString(),
-                            editTerm.getText().toString(),
+                            itemSpinner.getSelectedItem().toString(),
                             editAmount.getText().toString(),
                             editLocation.getText().toString());
                         if(isInserted=true) {
