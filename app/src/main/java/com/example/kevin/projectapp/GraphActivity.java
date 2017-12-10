e3eb67f3e684cef287daa49d1779b25441ce0383
THaoChen <es921155@gmail.com>
2017-12-10 17:36:50 +0800

    chart demo

--- app/src/main/java/com/example/kevin/projectapp/GraphActivity.java
+++ app/src/main/java/com/example/kevin/projectapp/GraphActivity.java
@@ -1,14 +1,70 @@
 package com.example.kevin.projectapp;
 
+import android.database.Cursor;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GraphActivity extends AppCompatActivity {
+
+    private static String TAG = "GraphActivity";
+
+    private float[] yData = {25.3f, 10.6f, 66.76f, 44.32f, 46.01f, 16.89f, 23.9f};
+    private String[] xData = {"Mitch", "Jessica" , "Mohammad" , "Kelsey", "Sam", "Robert", "Ashley"};
+    PieChart pieChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
+        //Log.d(TAG, "onCreate: starting to create chart");
 
+        pieChart = (PieChart) findViewById(R.id.PieChart);
+
+        //pieChart.setDescription("wrong");
+        pieChart.setRotationEnabled(true);
+        //pieChart.setUsePercentValues(true);
+        //pieChart.setHoleColor(Color.BLUE);
+        //pieChart.setCenterTextColor(Color.BLACK);
+        pieChart.setHoleRadius(25f);
+        pieChart.setTransparentCircleAlpha(0);
+        pieChart.setCenterText("Test Chart");
+        pieChart.setCenterTextSize(15);
+        //pieChart.setDrawEntryLabels(true);
+        //pieChart.setEntryLabelTextSize(20);
+        //More options just check out the documentation!
+
+        addDataSet();
     }
+
+    private void addDataSet() {
+        List<PieEntry> pieEntries = new ArrayList<>();
+        for(int i = 0;i < yData.length;i++){
+            pieEntries.add(new PieEntry(yData[i],xData[i]));
+        }
+
+        PieDataSet dataSet = new PieDataSet(pieEntries,"test");
+        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        PieData data = new PieData(dataSet);
+
+        PieChart chart = (PieChart)findViewById(R.id.PieChart);
+        chart.setData(data);
+        chart.invalidate();
+    }
+
 }
