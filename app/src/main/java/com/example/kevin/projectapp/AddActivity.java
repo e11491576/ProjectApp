f2206d1ac32d47ab7b18b0871b1a85b047a72047
THaoChen <es921155@gmail.com>
2017-12-25 14:02:42 +0800

    short GPS

--- app/src/main/java/com/example/kevin/projectapp/AddActivity.java
+++ app/src/main/java/com/example/kevin/projectapp/AddActivity.java
@@ -4,22 +4,16 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.database.Cursor;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -27,6 +21,7 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -187,6 +182,9 @@ public class AddActivity extends AppCompatActivity{
 
                 double latti = location.getLatitude();
                 double longi = location.getLongitude();
+                DecimalFormat df = new DecimalFormat("##.0000");
+                latti = Double.parseDouble(df.format(latti));
+                longi = Double.parseDouble(df.format(longi));
                 String consumeLocation = String.valueOf(latti) + "," + String.valueOf(longi);
                 editLocation.setText(consumeLocation);
             } else {
@@ -212,5 +210,7 @@ public class AddActivity extends AppCompatActivity{
 //        locationManager.setTestProviderEnabled(provider, false);
         super.onDestroy();
     }
+
+
 
 }
