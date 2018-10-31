0dc490934e9b72236c477849e5f6ab54b3680da2
Kevin <e11491576@gmail.com>
2018-10-31 18:03:56 +0800

    refactor delete button in Search

--- app/src/main/java/com/example/kevin/projectapp/SearchActivity.java
+++ app/src/main/java/com/example/kevin/projectapp/SearchActivity.java
@@ -3,30 +3,34 @@ package com.example.kevin.projectapp;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.provider.Settings;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.SimpleCursorAdapter;
 
 public class SearchActivity extends AppCompatActivity {
-    Button btnDelete;
-    EditText editTextId;
+    ListView list;
+    ImageButton btnDelete;
     DatabaseHelper myDb = new DatabaseHelper(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        ListView list = (ListView) findViewById(R.id.list);
         Cursor res = myDb.getAllData();
-        btnDelete=(Button)findViewById(R.id.btndelete);
-        editTextId=(EditText)findViewById(R.id.delete_id);
+        list = (ListView) findViewById(R.id.list);
         SimpleCursorAdapter adapter = new SimpleCursorAdapter
                 (this,R.layout.finance_row,
                         res,
@@ -35,14 +39,18 @@ public class SearchActivity extends AppCompatActivity {
                         0);
         list.setAdapter(adapter);
 
-        DeleteData();
     }
-    public void DeleteData() {
-        btnDelete.setOnClickListener(
+
+
+    public void myClickHandler(View v) {
+
+        v.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        Integer deletedRows = myDb.deleteData(editTextId.getText().toString());
+                        ConstraintLayout vwParentRow = (ConstraintLayout)v.getParent();
+                        TextView child = (TextView)vwParentRow.getChildAt(0);
+                        Integer deletedRows = myDb.deleteData(child.getText().toString());
                         if(deletedRows > 0)
                         {
                             Toast.makeText(SearchActivity.this, "資料刪除成功", Toast.LENGTH_LONG).show();
@@ -57,7 +65,8 @@ public class SearchActivity extends AppCompatActivity {
                     }
                 }
         );
-
     }
 
 }
+
+
